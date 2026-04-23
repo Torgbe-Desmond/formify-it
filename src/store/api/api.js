9a01fefdf9b43b3c7c86e63642db99f8ff97935c
123torgbe@gmail.com <123torgbe@gmail.com>
2026-04-23 09:56:00 +0000

    FIX: changed the compare operator from == to === in api.js

--- src/store/api/api.js
+++ src/store/api/api.js
@@ -1,6 +1,6 @@
 function environmentType(environment){
     const env = "Production"
-    if(environment.toLowerCase() == env.toLowerCase()){
+    if(environment.toLowerCase() === env.toLowerCase()){
         return "https://formify-node-3kzc.onrender.com"
     } else {
         return "http://localhost:5000"
